27a1edca95fc8b3dad1d44fdf266de483823cb1e
Robert Taylor <rjt903@gmail.com>
2023-11-17 15:52:24 +0000

    designed task class

---
+++ lib/task.py
@@ -0,0 +1,24 @@
+class Task():
+    #user facing properties
+    #   task: String
+    #   complete: Bool
+
+    def __init__(self, task):
+        # Parameters
+        #   task: String
+        #   complete: Bool
+        # Returns
+        #   nothing
+        # Side effects
+        #   sets the task and complete properties
+        self.task = task
+        self.complete = False
+
+    def complete_task(self):
+        # Parameters
+        #   none
+        # Returns
+        #   none
+        # Side effects
+        #   set complete to True
+        pass
