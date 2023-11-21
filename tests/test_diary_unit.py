68412720dddfaca348ad8c2f543d066ca32601e4
Robert Taylor <rjt903@gmail.com>
2023-11-21 11:12:12 +0000

    Added unit tests using Mock()

--- tests/test_diary_unit.py
+++ tests/test_diary_unit.py
@@ -1,4 +1,5 @@
 from lib.diary import *
+from unittest.mock import Mock
 
 '''
 Given I have a diary
@@ -16,3 +17,45 @@ Retrieve entries returns an empty list
 def test_retrieve_returns_empty():
     diary = Diary()
     assert diary.retrieve_entries() == []
+
+'''
+Given I have a diary
+I can add an entry and see it in my list of entries
+'''
+def test_add_entry_show_list():
+    diary = Diary()
+    fake_entry = Mock()
+    fake_entry.title = 'Day Seven'
+    fake_entry.contents = 'Week two is going great, I love bootcamp!'
+    diary.add_diary_entry(fake_entry)
+    assert diary.entries == [fake_entry]
+
+'''
+Given I have a diary with entries
+I can retrieve a formatted list of entries
+'''
+def test_retrieve_formatted_entries():
+    diary = Diary()
+    fake_entry = Mock()
+    fake_entry.format.return_value = 'Day Eight: This is so hard what am I doing here?'
+    diary.add_diary_entry(fake_entry)
+    assert diary.retrieve_entries() == ['Day Eight: This is so hard what am I doing here?']
+
+'''
+Given I have a diary with entries
+I can tell how much time I have and retrieve an entry I can read in time
+'''
+def test_retrieve_entry_based_off_reading_time():
+    diary = Diary()
+    fake_entry_01 = Mock()
+    fake_entry_02 = Mock()
+    fake_entry_01.title = 'Day Seven'
+    fake_entry_01.contents = 'Week two is going great, I love bootcamp!'
+    fake_entry_01.reading_time.return_value = 1
+    fake_entry_01.format.return_value = 'Day Seven: Week two is going great, I love bootcamp!'
+    fake_entry_02.title = 'Day Eight'
+    fake_entry_02.contents = 'This is so hard what am I doing here?'
+    fake_entry_02.reading_time.return_value = 3
+    diary.add_diary_entry(fake_entry_02)
+    diary.add_diary_entry(fake_entry_01)
+    assert diary.select_entry_for_time(1, 50) == 'Day Seven: Week two is going great, I love bootcamp!'
